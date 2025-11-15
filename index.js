54c592eb7a03b8c0d5bc858ed52edb6c9d8a8579
RavchanBest <isokov.ravshan31@gmail.com>
2025-11-15 03:34:10 -0800

    first push

---
+++ index.js
@@ -0,0 +1,30 @@
+let name = "Ravshan"
+let age = 10
+let isHungry = false
+let clicks = 0
+
+
+let btn = document.querySelector(".ctabtn")
+let clicksdisplay = document.querySelector(".clicks")
+
+
+
+
+function checkAge() {
+    if (age => 18) {
+        console.log("добро пожаловать!")
+    }
+    else {
+        console.log("вход запрещён!")
+    }
+}
+
+
+
+
+btn.addEventListener("click", () => {
+    clicksdisplay.innerHTML = clicks += 1000000
+})
+
+
+
